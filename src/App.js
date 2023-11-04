a0c85807acc933a200cbdab0179cc803b4bd621f
Nishani Kasineshan <73964224+NishaniKasineshan@users.noreply.github.com>
2023-11-04 20:24:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import React from 'react';
+import ImageFileUpload from './modules/ImageFileUpload';
 import FileUpload from './modules/FileUpload'
 import Sita from './modules/title';
 import Download from './modules/download';
 
 
 function App() {
+
   return (
     <div>
       <Sita/>
+      <ImageFileUpload/>
       <FileUpload/>
       <Download></Download>
     </div>
