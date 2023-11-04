e08844cf9cb5bc8773feb40cd6af263e6979176d
Nishani Kasineshan <e18245@eng.pdn.ac.lk>
2023-11-04 20:19:59 +0530

    "updated last"

--- modules/index.js
+++ modules/index.js
@@ -4,6 +4,7 @@ const multer=require('multer');
 const preprocess_mod=require('./preprocess');
 const ocr_mod=require('./ocr');
 const translate_mod=require('./translate');
+const path =require('path');
 
 //storing images to disk
 const storage=multer.diskStorage({
@@ -14,25 +15,46 @@ const storage=multer.diskStorage({
         cb(null,file.originalname);
     }
 })
+var curr=null;
 
-const upload=multer({storage:storage})
+const upload=multer({storage:storage});
+const allowedImgaeExtensions = ['.jpg', '.png','.jpeg'];
 
-app.post('/api/upload',upload.single('uploadedImage'),async (req,res)=>{
+app.post('/upload',upload.single('uploadedImage'),async (req,res)=>{
     console.log(req.file);
+    const fileExtension=path.extname(req.file.filename).toLowerCase();
+    curr=req.file.filename.split(".")[0]+'.txt';
+
     try {
-        preprocess_mod.preprocess_image(req.file.filename)
-            .then( img_buffer => {
+        if(allowedImgaeExtensions.includes(fileExtension)){
+            console.log('File extension is allowed:', fileExtension);
+            preprocess_mod.preprocess_image(req.file.filename).then( img_buffer => {
                 return ocr_mod.ocr_extract(img_buffer);                   
             }).then( text =>{
-                return translate_mod.translateText(text);
+                return translate_mod.translateText(text,req.file.filename);
             }).then( translated_text => {
                 return res.json({
-                        message:translated_text
+                    message:translated_text
                 });
-            })       
+            })  
+        } else {
+            console.log('File extension is not allowed:', fileExtension);
+        }     
     } catch (error) {
         console.error(error);
     }
+})
+
+app.get('/download',async (req,res)=>{
+    res.download('../tmp/translations/'+curr,"download.txt",(err)=>{
+        if(err){
+            res.send({
+                error:err,
+                msg:"Problem downloading the file"
+            })
+        }
+    });
+    
 })
 
 app.listen(4000,()=>{
--- modules/translate.js
+++ modules/translate.js
@@ -1,13 +1,17 @@
 const translate =require('@iamtraction/google-translate');
 const fs= require('fs-extra');
 
-async function translateText(srcfile){
+async function translateText(srcfile,filename){
     // const src_text=fs.readFileSync('tmp/ocr_results/'+srcfile,{ encoding: 'utf8', flag: 'r' });
     return new Promise(async(resolve,reject)=>{
         try {
             translate(srcfile,{from:'si', to:'ta'}
             ).then(res =>{
-                fs.writeFileSync('../tmp/translations/translation.txt',res.text,{encoding:'utf8',flag:'w'});
+                fs.appendFile('../tmp/translations/'+filename.split(".")[0]+'.txt',res.text,'utf8',err=>{
+                    if(err){
+                        throw err;
+                    }
+                });
                 resolve(res.text)
             })
         } catch (error) {
