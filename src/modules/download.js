281cd2bf6e08c18c8e954943aaada3d665c7c837
Nishani Kasineshan <73964224+NishaniKasineshan@users.noreply.github.com>
2023-11-04 20:21:35 +0530

    Update download.js

--- src/modules/download.js
+++ src/modules/download.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import './download.css'
+import fileDownload from 'js-file-download'
+import axios from 'axios';
+
 
 export default function Download() {
+
+  const handleFileDownload = async () => {
+    try {
+      axios.get('http://localhost:4000/download', {
+        responseType: 'blob', // Important for binary data like files
+      }).then((res)=>{
+        fileDownload(res.data)
+      });
+
+     
+    } catch (error) {
+      console.error('Error downloading file:', error);
+    }
+  };
   return (
     <div className='download'>
-       <button>Download</button>
+       <button onClick={handleFileDownload}>Download</button>
     </div>
   )
 }
